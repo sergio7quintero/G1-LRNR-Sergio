30c348432026abae75b1b6b9441f3158069f8d68
JaredP872 <jared.proctor7642@gmail.com>
2025-02-12 19:12:41 -0500

    feat: Adjust sizing of the h1, p and button elements on the Results page.

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Result() {
   return (
     <div className="flex flex-col justify-center items-center mt-5 h-[80vh]">
-      <h1 className="text-4xl text-green-600 font-medium mb-5">Irnr</h1>
-      <p className="text-lg font-medium mb-10">Questions Right: 0111</p>
+      <h1 className="text-7xl text-green-600 font-medium mb-5">Irnr</h1>
+      <p className="text-2xl font-medium mb-10">Questions Right: 0111</p>
 
-      <button className="bg-green-600 text-white font-bold py-2 px-4 ">
+      <button className="bg-green-600 text-white font-medium py-3 px-4 ">
         TRY ANOTHER QUIZ
       </button>
     </div>
