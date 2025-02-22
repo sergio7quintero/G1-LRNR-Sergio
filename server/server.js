35a807ec7d8f0a733a7db0725aae7e3dfe7b6521
Nathaniel L. Sherer <NateSherer@Administrators-MacBook-Pro.local>
2025-02-22 17:53:16 -0500

    Resolved a fetching conflict to make sure questions were being fetched

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const OpenAI = require('openai')
+const dotenv = require('dotenv') 
+const path = require('path');
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 
 const app = express()
 const PORT = 5000
 
 app.use(cors({
     origin: 'http://localhost:3000'
-}));
+})); 
 
 //Making call to open ai to fetch questions
 app.get('/api/questions', async (req, res) => {
 // API key below
-    const OPENAI_API_KEY = "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"
-    const openai = new OpenAI({apiKey: OPENAI_API_KEY})
+    const apiKey = process.env.OPENAI_API_KEY; 
+    const openai = new OpenAI({ apiKey: apiKey });
     // AI model
     const aiModel = "gpt-4o"
     //Messages Array
@@ -22,7 +26,7 @@ app.get('/api/questions', async (req, res) => {
             role: "system",
             content: 'you are a quiz master. generate 5 random questions with 4 multiple choice answers. Also provide the answer seperately. The response should be in the following json format: {"questions":[{"id":0,"questions":""options":[]"answer":""},...]}'
         }
-    ]
+    ] 
     try {
         const completion = await openai.chat.completions.create({
             model: aiModel,
@@ -40,5 +44,6 @@ app.get('/api/questions', async (req, res) => {
     res.status(500).send('Error fetching questions from OpenAI');
 }
 })
+
 
 app.listen(PORT, ()=> {console.log('server is running on port 5000 ')})
