a53cd3d7852b21e88de59786cede898bdd96ec9f
Nathaniel L. Sherer <NateSherer@Administrators-MacBook-Pro.local>
2025-02-22 17:01:42 -0500

    Set up the backend to fetch questions using API key

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const cors = require('cors')
+const OpenAI = require('openai')
+
+const app = express()
+const PORT = 5000
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+//Making call to open ai to fetch questions
+app.get('/api/questions', async (req, res) => {
+// API key below
+    const OPENAI_API_KEY = "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"
+    const openai = new OpenAI({apiKey: OPENAI_API_KEY})
+    // AI model
+    const aiModel = "gpt-4o"
+    //Messages Array
+    const messages = [
+        {
+            role: "system",
+            content: 'you are a quiz master. generate 5 random questions with 4 multiple choice answers. Also provide the answer seperately. The response should be in the following json format: {"questions":[{"id":0,"questions":""options":[]"answer":""},...]}'
+        }
+    ]
+    try {
+        const completion = await openai.chat.completions.create({
+            model: aiModel,
+            messages: messages,
+        });
+
+    const aiResponse = completion.choices[0].message.content
+
+//Parse string into json
+//return the question json
+    const json = JSON.parse(aiResponse)
+    res.json(json);
+} catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching questions from OpenAI');
+}
+})
+
+app.listen(PORT, ()=> {console.log('server is running on port 5000 ')})
