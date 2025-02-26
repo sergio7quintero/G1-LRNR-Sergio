21624bd0d2ad97e6bcf5d7ef7f2b12cd4a39d630
JaredP872 <jared.proctor7642@gmail.com>
2025-02-26 13:52:27 -0500

    Feat: Change color and hover effect of the result page button to green and dark green when hovered over.

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -6,7 +6,7 @@ export default function Result() {
       <h1 className="text-7xl text-teal-600 font-medium mb-5">Irnr</h1>
       <p className="text-2xl font-medium mb-10">Questions Right: 0111</p>
 
-      <button className="bg-teal-600 text-white font-medium py-3 px-4 ">
+      <button className="bg-emerald-500 text-white font-medium py-3 px-4  hover:bg-emerald-600  cursor-pointer">
         TRY ANOTHER QUIZ
       </button>
     </div>
